8aeb641e8e57cf02f91de7ca035dbd6093b3359e
mininet <mininet@mininet-vm.(none)>
2014-09-16 14:32:57 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+SDN_lab1
+=======
+
+COMS E6998-10 Fall 2014 Software Defined Networking Lab 1
---
+++ src/main/edu/columbia/cs6998/sdn/lab1/Lab1Hub.java
@@ -0,0 +1,230 @@
+/**
+*    Copyright 2014, Columbia University.
+*    Lab1, COMS E6998-10 Fall 2014
+*    Software Defined Networking
+*    Originally created by Shangjin Zhang, Columbia University
+* 
+*    Licensed under the Apache License, Version 2.0 (the "License"); you may
+*    not use this file except in compliance with the License. You may obtain
+*    a copy of the License at
+*
+*         http://www.apache.org/licenses/LICENSE-2.0
+*
+*    Unless required by applicable law or agreed to in writing, software
+*    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+*    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+*    License for the specific language governing permissions and limitations
+*    under the License.
+**/
+
+/**
+ * Floodlight
+ * A BSD licensed, Java based OpenFlow controller
+ *
+ * Floodlight is a Java based OpenFlow controller originally written by David Erickson at Stanford
+ * University. It is available under the BSD license.
+ *
+ * For documentation, forums, issue tracking and more visit:
+ *
+ * http://www.openflowhub.org/display/Floodlight/Floodlight+Home
+ **/
+
+package edu.columbia.cs6998.sdn.lab1;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import net.floodlightcontroller.core.FloodlightContext;
+import net.floodlightcontroller.core.IFloodlightProviderService;
+import net.floodlightcontroller.core.IOFMessageListener;
+import net.floodlightcontroller.core.IOFSwitch;
+import net.floodlightcontroller.core.module.FloodlightModuleContext;
+import net.floodlightcontroller.core.module.FloodlightModuleException;
+import net.floodlightcontroller.core.module.IFloodlightModule;
+import net.floodlightcontroller.core.module.IFloodlightService;
+import net.floodlightcontroller.packet.Ethernet;
+
+import org.openflow.protocol.OFError;
+import org.openflow.protocol.OFFlowMod;
+import org.openflow.protocol.OFFlowRemoved;
+import org.openflow.protocol.OFMatch;
+import org.openflow.protocol.OFMessage;
+import org.openflow.protocol.OFPacketIn;
+import org.openflow.protocol.OFPacketOut;
+import org.openflow.protocol.OFPort;
+import org.openflow.protocol.OFType;
+import org.openflow.protocol.action.OFAction;
+import org.openflow.protocol.action.OFActionOutput;
+import org.openflow.util.HexString;
+import org.openflow.util.LRULinkedHashMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Lab1Hub 
+    implements IFloodlightModule, IOFMessageListener {
+    protected static Logger log = LoggerFactory.getLogger(Lab1Hub.class);
+    
+    // Module dependencies
+    protected IFloodlightProviderService floodlightProvider;
+
+    // flow-mod - for use in the cookie
+    public static final int LAB1_HUB_APP_ID = 10;
+    // LOOK! This should probably go in some class that encapsulates
+    // the app cookie management
+    public static final int APP_ID_BITS = 12;
+    public static final int APP_ID_SHIFT = (64 - APP_ID_BITS);
+    public static final long LAB1_HUB_COOKIE = (long) (LAB1_HUB_APP_ID & ((1 << APP_ID_BITS) - 1)) << APP_ID_SHIFT;
+    
+    /**
+     * @param floodlightProvider the floodlightProvider to set
+     */
+    public void setFloodlightProvider(IFloodlightProviderService floodlightProvider) {
+        this.floodlightProvider = floodlightProvider;
+    }
+    
+    @Override
+    public String getName() {
+        return "Lab1Hub";
+    }
+
+    /**
+     * Writes an OFPacketOut message to a switch.
+     * @param sw The switch to write the PacketOut to.
+     * @param packetInMessage The corresponding PacketIn.
+     * @param egressPort The switchport to output the PacketOut.
+     */
+    private void writePacketOutForPacketIn(IOFSwitch sw, 
+                                          OFPacketIn packetInMessage, 
+                                          short egressPort) {
+
+        // from openflow 1.0 spec - need to set these on a struct ofp_packet_out:
+        // uint32_t buffer_id; /* ID assigned by datapath (-1 if none). */
+        // uint16_t in_port; /* Packet's input port (OFPP_NONE if none). */
+        // uint16_t actions_len; /* Size of action array in bytes. */
+        // struct ofp_action_header actions[0]; /* Actions. */
+        /* uint8_t data[0]; */ /* Packet data. The length is inferred
+                                  from the length field in the header.
+                                  (Only meaningful if buffer_id == -1.) */
+        
+        OFPacketOut packetOutMessage = (OFPacketOut) floodlightProvider.getOFMessageFactory().getMessage(OFType.PACKET_OUT);
+        short packetOutLength = (short) OFPacketOut.MINIMUM_LENGTH; // starting length
+
+        // Set buffer_id, in_port, actions_len
+        packetOutMessage.setBufferId(packetInMessage.getBufferId());
+        packetOutMessage.setInPort(packetInMessage.getInPort());
+        packetOutMessage.setActionsLength((short) OFActionOutput.MINIMUM_LENGTH);
+        packetOutLength += OFActionOutput.MINIMUM_LENGTH;
+        
+        // set actions
+        List<OFAction> actions = new ArrayList<OFAction>(1);      
+        actions.add(new OFActionOutput(egressPort, (short) 0));
+        packetOutMessage.setActions(actions);
+
+        // set data - only if buffer_id == -1
+        if (packetInMessage.getBufferId() == OFPacketOut.BUFFER_ID_NONE) {
+            byte[] packetData = packetInMessage.getPacketData();
+            packetOutMessage.setPacketData(packetData); 
+            packetOutLength += (short) packetData.length;
+        }
+        
+        // finally, set the total length
+        packetOutMessage.setLength(packetOutLength);              
+            
+        // and write it out
+        try {
+            sw.write(packetOutMessage, null);
+        } catch (IOException e) {
+            log.error("Failed to write {} to switch {}: {}", new Object[] { packetOutMessage, sw, e });
+        }
+    }
+    
+    /**
+     * Processes a OFPacketIn message. If the switch has learned the MAC to port mapping
+     * for the pair it will write a FlowMod for. If the mapping has not been learned the 
+     * we will flood the packet.
+     * @param sw
+     * @param pi
+     * @param cntx
+     * @return
+     */
+    private Command processPacketInMessage(IOFSwitch sw, OFPacketIn pi, FloodlightContext cntx) {
+
+        return Command.CONTINUE;
+    }
+
+    // IOFMessageListener
+    
+    @Override
+    public Command receive(IOFSwitch sw, OFMessage msg, FloodlightContext cntx) {
+        switch (msg.getType()) {
+            case PACKET_IN:
+                return this.processPacketInMessage(sw, (OFPacketIn) msg, cntx);
+            case ERROR:
+                log.info("received an error {} from switch {}", (OFError) msg, sw);
+                return Command.CONTINUE;
+            default:
+                break;
+        }
+        log.error("received an unexpected message {} from switch {}", msg, sw);
+        return Command.CONTINUE;
+    }
+
+    @Override
+    public boolean isCallbackOrderingPrereq(OFType type, String name) {
+        return false;
+    }
+
+    @Override
+    public boolean isCallbackOrderingPostreq(OFType type, String name) {
+        return false;
+    }
+
+    // IFloodlightModule
+    
+    @Override
+    public Collection<Class<? extends IFloodlightService>> getModuleServices() {
+        Collection<Class<? extends IFloodlightService>> l = 
+                new ArrayList<Class<? extends IFloodlightService>>();
+        return l;
+    }
+
+    @Override
+    public Map<Class<? extends IFloodlightService>, IFloodlightService>
+            getServiceImpls() {
+        Map<Class<? extends IFloodlightService>,
+            IFloodlightService> m = 
+                new HashMap<Class<? extends IFloodlightService>,
+                    IFloodlightService>();
+        return m;
+    }
+
+    @Override
+    public Collection<Class<? extends IFloodlightService>>
+            getModuleDependencies() {
+        Collection<Class<? extends IFloodlightService>> l = 
+                new ArrayList<Class<? extends IFloodlightService>>();
+        l.add(IFloodlightProviderService.class);
+        return l;
+    }
+
+    @Override
+    public void init(FloodlightModuleContext context)
+            throws FloodlightModuleException {
+        floodlightProvider =
+                context.getServiceImpl(IFloodlightProviderService.class);
+    }
+
+    @Override
+    public void startUp(FloodlightModuleContext context) {
+        floodlightProvider.addOFMessageListener(OFType.PACKET_IN, this);
+        floodlightProvider.addOFMessageListener(OFType.ERROR, this);
+    }
+}
